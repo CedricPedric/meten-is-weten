f0890baabd5d8e577dd960b3697d636c6dab7412
CedricPedric <99059069@mydavinci.nl>
2021-09-08 09:58:12 +0200

    input en if-statement

---
+++ meten-is-weten.py
@@ -0,0 +1,15 @@
+a = input("Voer hier getal A in: ")
+b = input("Voer hier getal B in: ")
+
+if a>b:
+    Max = a 
+    print("a is het grootste getal " + str(Max))
+
+elif a<b:
+    Min = a
+    print("a is het kleinste getal: " + str(Min))
+
+else: 
+    print("Laat het programma printen: ‘a en b zijn even groot")
+
+
